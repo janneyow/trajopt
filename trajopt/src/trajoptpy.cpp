a3474855cdb641d6b105709b4413ea46293c6ea5
J-Anne <janne.yow@gmail.com>
2023-07-21 10:18:19 +0800

    minor edits in comments

--- trajopt/src/trajoptpy.cpp
+++ trajopt/src/trajoptpy.cpp
@@ -208,12 +208,11 @@ Json::Value readJsonFile(const std::string& doc)
 PyTrajOptProb PyConstructProblem(const std::string& json_string, py::list pos, py::list joint_names)
 {    
     // Initialize the environment
-        // Initialize the environment
     std::string pkg_path = ros::package::getPath("trajopt_ros");
 
     std::string urdf_file_path = pkg_path + "/assets/xarm6/xarm6_with_gripper.urdf";
     std::string srdf_file_path = pkg_path + "/assets/xarm6/xarm6_with_gripper.srdf";
-    //
+
     std::cout << "URDF FILE PATH: " << urdf_file_path << std::endl;
 
     // for env setup, to write python class to replace this
